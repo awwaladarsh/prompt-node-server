9cc29689505195e277852fae1a1a1253ff9aae7a
Rao Vellanki <syam.rao.vellanki@accenture.com>
2017-06-08 19:26:25 +0530

    first

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(function(req, res, next) {
 var config = {
   dbServer: {
     protocol: 'http://',
-    host: 'localhost:5984',
+    host: 'http://c821a072.ngrok.io',
     user: '',
     password: '',
     userDB: 'sl-users',
